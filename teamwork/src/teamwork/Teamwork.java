04cec232b7c14ae96b62959e27919dbb8677e5f8
NicholasBergen <nicbergen472@gmail.com>
2022-09-28 10:22:37 -0500

    Added print line

--- teamwork/src/teamwork/Teamwork.java
+++ teamwork/src/teamwork/Teamwork.java
@@ -5,6 +5,9 @@ public class Teamwork {
 	public static void main(String[] args) {
 		// this is the one 
 		System.out.println("Hello Jacob");
+		
+		//hello my friend
+		System.out.println("Hello my friend"); 
 	}
 
 }
