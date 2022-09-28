a3a63edc5e203065af2c02578984b3eef7d8bc5d
Carter <carter.jgreid@gmail.com>
2022-09-28 10:37:28 -0500

    i hate this

--- teamwork/src/teamwork/Teamwork.java
+++ teamwork/src/teamwork/Teamwork.java
@@ -3,10 +3,10 @@ package teamwork;
 public class Teamwork {
 
 	public static void main(String[] args) {
-		// this is the one 
+		
 		System.out.println("Hello Jacob");
 		
-		//hello my friend balls
+		
 		System.out.println("Hello my friend"); 
 		
 		System.out.println("Jakey Sawa sucks");
